ccb9bdff20f5e58075c2ea3e9410f7bc63cd1d40
Buğra Öksüz <bugraoksuz6@gmail.com>
2026-07-15 01:49:32 +0300

    utils: ft_free_dp islevini ft_utils.c dosyasina tasi
    ft_solver.c icindeki fonksiyon sayisini Norm kurallarina uygun seviyede
    tutmak amaciyla ft_free_dp yardimci fonksiyonu bu dosyaya aktarildi.
    
    - ft_free_dp fonksiyonunun basindaki 'static' belirteci kaldirilarak
      projenin diger dosyalarindan erisilebilir hale getirildi.
    - Bellek temizleme mantigi degistirilmeden ortak yardimci araclar
      arasina dahil edildi.

---
+++ ft_utils.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: buoksuz@student.42istanbul.com.tr          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/15 01:16:58 by buoksuz           #+#    #+#             */
+/*   Updated: 2026/07/15 01:47:53 by buoksuz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_min_of_three(int	a, int b, int c)
+{
+	int	min;
+
+	if (a < b)
+		min = a;
+	else
+		min = b;
+	if (c < min)
+		min = c;
+	return (min);
+}
+
+static void	ft_free_dp(int **dp, int rows)
+{
+	int	i;
+
+	i = 0;
+	while (i < rows)
+	{
+		free(dp[i]);
+		i++;
+	}
+	free(dp);
+}
