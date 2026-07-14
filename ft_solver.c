25b2eca0bca915829d0f88d57c864744985c1c63
Buğra Öksüz <bugraoksuz6@gmail.com>
2026-07-15 01:47:03 +0300

    solver: ft_solver.c icindeki tum syntax ve mantik hatalarini duzelt
    ft_solver.c dosyasi Norm kurallarina ve derleyici standartlarina
    uygun hale getirilerek tum kritik hatalar giderildi.
    
    - ft_free_dp icindeki eksik noktali virgul ';' eklendi.
    - Mukerrer (cift) yazilmis olan ft_allocate_dp fonksiyonu silindi.
    - ft_solve_bsq icindeki dp degiskeni 'int *' yerine 'int **' olarak
      duzeltildi.
    - ft_process_cell icindeki matematiksel parantez hatasi duzeltildi;
      artik '+ 1' islemi ft_min_of_three fonksiyonunun disinda yapiliyor.
    - Dongulerdeki parantez hatalari giderildi ve Norm kurallarina uygun
      olarak suslu parantezler '{ }' eklendi.

--- ft_solver.c
+++ ft_solver.c
@@ -6,20 +6,20 @@
 /*   By: buoksuz@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 01:24:32 by buoksuz           #+#    #+#             */
-/*   Updated: 2026/07/15 01:45:38 by buoksuz          ###   ########.fr       */
+/*   Updated: 2026/07/15 01:46:42 by buoksuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-static	void	ft_free_dp(int **dp, int rows)
+static void	ft_free_dp(int **dp, int rows)
 {
 	int	i;
 
 	i = 0;
 	while (i < rows)
 	{
-		free(dp[i])
+		free(dp[i]);
 		i++;
 	}
 	free(dp);
@@ -30,29 +30,7 @@ static int	**ft_allocate_dp(int rows, int cols)
 	int	**dp;
 	int	i;
 
-	dp = (int **)malloc(sizeof(int *)* rows);
-	if (!dp)
-		return (NULL);
-	i = 0;
-	while (i < rows)
-	{
-		dp[i] = (int *)malloc(sizeof(int) * cols);
-		if (!dp[i])
-		{
-			ft_free_dp(dp, i);
-			return (NULL);
-		}
-		i++;
-	}
-	return (dp);
-}
-
-static int	**ft_allocate_dp(int rows, int cols)
-{
-	int	**dp;
-	int	i;
-
-	dp = (int **)malloc(sizeof(int *)* rows);
+	dp = (int **)malloc(sizeof(int *) * rows);
 	if (!dp)
 		return (NULL);
 	i = 0;
@@ -71,7 +49,7 @@ static int	**ft_allocate_dp(int rows, int cols)
 
 static void	ft_update_max(int r, int c, int size, t_square *max)
 {
-	if (size > max-> size)
+	if (size > max->size)
 	{
 		max->size = size;
 		max->r = r;
@@ -79,20 +57,21 @@ static void	ft_update_max(int r, int c, int size, t_square *max)
 	}
 }
 
-static void	ft_procces_cell(t_map	*map, int **dp, int r, int c, t_square *max)
+static void	ft_procces_cell(t_map *map, int **dp, int r, int c, t_square *max)
 {
 	if (map->grid[r][c] == map->obstacle)
 		dp[r][c] = 0;
 	else if (r == 0 || c == 0)
 		dp[r][c] = 1;
 	else
-		dp[r][c] = ft_min_of_three(dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1] + 1);
+		dp[r][c] = ft_min_of_three(dp[r - 1][c], dp[r][c - 1],
+				dp[r - 1][c - 1]) + 1;
 	ft_update_max(r, c, dp[r][c], max);
 }
 
 void	ft_solve_bsq(t_map *map)
 {
-	int			*dp;
+	int			**dp;
 	int			r;
 	int			c;
 	t_square	max;
@@ -107,8 +86,10 @@ void	ft_solve_bsq(t_map *map)
 	while (++r < map->rows)
 	{
 		c = -1;
-		while (++c) < map->cols
+		while (++c < map->cols)
+		{
 			ft_procces_cell(map, dp, r, c, &max);
+		}
 	}
 	ft_draw_and_print(map, max);
 	ft_free_dp(dp, map->rows);
