ad95fd79618bc8085361cdb7bc50d05ee2715a61
Buğra Öksüz <bugraoksuz6@gmail.com>
2026-07-15 01:39:16 +0300

    solver: Dinamik programlama matrisi icin bellek ayirma islevini ekle
    Dinamik programlama tablosu (DP) olarak kullanilacak iki boyutlu tam sayi
    matrisini guvenli bir sekilde olusturmak amaciyla ft_allocate_dp()
    yardimci fonksiyonu eklendi.
    
    - rows x cols boyutunda bellek tahsis edilir.
    - Tahsis isleminin herhangi bir satirinda basarisiz olunursa (malloc hatasi),
      o ana kadar ayrilmis olan tum bellek sızıntı (memory leak) olusmadan
      ft_free_dp yardimiyla serbest birakilir ve NULL donulur.

--- ft_solver.c
+++ ft_solver.c
@@ -6,7 +6,7 @@
 /*   By: buoksuz@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 01:24:32 by buoksuz           #+#    #+#             */
-/*   Updated: 2026/07/15 01:37:43 by buoksuz          ###   ########.fr       */
+/*   Updated: 2026/07/15 01:38:45 by buoksuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,28 @@ static	void	ft_free_dp(int **dp, int rows)
 		i++;
 	}
 	free(dp);
+}
+
+static int	**ft_allocate_dp(int rows, int cols)
+{
+	int	**dp;
+	int	i;
+
+	dp = (int **)malloc(sizeof(int *)* rows);
+	if (!dp)
+		return (NULL);
+	i = 0;
+	while (i < rows)
+	{
+		dp[i] = (int *)malloc(sizeof(int) * cols);
+		if (!dp[i])
+		{
+			ft_free_dp(dp, i);
+			return (NULL);
+		}
+		i++;
+	}
+	return (dp);
 }
 
 static int	**ft_allocate_dp(int rows, int cols)
