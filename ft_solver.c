b362e2b282fed21d4105d82fc948a69871830644
Buğra Öksüz <bugraoksuz6@gmail.com>
2026-07-15 02:01:35 +0300

    solver: TOO_MANY_ARGS Norm hatasini gidermek icin koordinatlari paketle
    ft_procces_cell fonksiyonunun 5 arguman almasindan kaynaklanan Norm hatasi,
    r ve c koordinatlarinin tek bir struct icinde birlestirilmesiyle giderildi.
    
    - Dongu degiskenleri 'int r, c' yerine 't_square curr' yapisina tasindi.
    - ft_procces_cell fonksiyonunun parametre sayisi 4'e dusuruldu.
    - ft_update_max fonksiyonu yeni 'curr' yapisini okuyacak sekilde guncellendi.

--- ft_solver.c
+++ ft_solver.c
@@ -6,7 +6,7 @@
 /*   By: buoksuz@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 01:24:32 by buoksuz           #+#    #+#             */
-/*   Updated: 2026/07/15 01:57:17 by buoksuz          ###   ########.fr       */
+/*   Updated: 2026/07/15 02:00:45 by buoksuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,18 +34,23 @@ static int	**ft_allocate_dp(int rows, int cols)
 	return (dp);
 }
 
-static void	ft_update_max(int r, int c, int size, t_square *max)
+static void	ft_update_max(t_square curr, int size, t_square *max)
 {
 	if (size > max->size)
 	{
 		max->size = size;
-		max->r = r;
-		max->c = c;
+		max->r = curr.r;
+		max->c = curr.c;
 	}
 }
 
-static void	ft_procces_cell(t_map *map, int **dp, int r, int c, t_square *max)
+static void	ft_procces_cell(t_map *map, int **dp, t_square curr, t_square *max)
 {
+	int	r;
+	int	c;
+
+	r = curr.r;
+	c = curr.c;
 	if (map->grid[r][c] == map->obstacle)
 		dp[r][c] = 0;
 	else if (r == 0 || c == 0)
@@ -53,14 +58,13 @@ static void	ft_procces_cell(t_map *map, int **dp, int r, int c, t_square *max)
 	else
 		dp[r][c] = ft_min_of_three(dp[r - 1][c], dp[r][c - 1],
 				dp[r - 1][c - 1]) + 1;
-	ft_update_max(r, c, dp[r][c], max);
+	ft_update_max(curr, dp[r][c], max);
 }
 
 void	ft_solve_bsq(t_map *map)
 {
 	int			**dp;
-	int			r;
-	int			c;
+	t_square	curr;
 	t_square	max;
 
 	dp = ft_allocate_dp(map->rows, map->cols);
@@ -69,13 +73,13 @@ void	ft_solve_bsq(t_map *map)
 	max.size = 0;
 	max.r = 0;
 	max.c = 0;
-	r = -1;
-	while (++r < map->rows)
+	curr.r = -1;
+	while (++curr.r < map->rows)
 	{
-		c = -1;
-		while (++c < map->cols)
+		curr.c = -1;
+		while (++curr.c < map->cols)
 		{
-			ft_procces_cell(map, dp, r, c, &max);
+			ft_procces_cell(map, dp, curr, &max);
 		}
 	}
 	ft_draw_and_print(map, max);
